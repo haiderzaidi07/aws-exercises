556dafbb9d215ec3e4b301617acd6975c6509612
haiderzaidi07 <haiderzaidi.work@gmail.com>
2025-07-30 18:18:16 +0500

    added script and null resource for zip creation

--- task11/s3/main.tf
+++ task11/s3/main.tf
@@ -12,4 +12,17 @@ resource "aws_s3_object" "haider-tf-beanstalk-object" {
   bucket = aws_s3_bucket.haider-tf-beanstalk-bucket.id
   key    = "haider-eb-nodejs.zip"
   source = "./app/haider-eb-nodejs.zip"
+
+  depends_on = [ null_resource.create_nodejs_app_zip ]
+}
+
+resource "null_resource" "create_nodejs_app_zip" {
+
+  provisioner "local-exec" {
+    command = file("${path.root}/create_app_zip.sh")
+  }
+
+  triggers = {
+    "run_at" = timestamp()
+  }
 }
