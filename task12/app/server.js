48b5738c68bfe85a18a7e6f544c736e58fdaa845
haiderzaidi07 <haiderzaidi.work@gmail.com>
2025-07-31 22:04:15 +0500

    made change to app

--- task12/app/server.js
+++ task12/app/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Elastic Beanstalk!');
+  res.send('Hello Elastic Beanstalk from CodePipeline!');
 });
 
 app.listen(PORT, () => {
