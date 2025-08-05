2fd3575ca1238e389bba8f968d7113faa5c1d2bb
haiderzaidi07 <haiderzaidi.work@gmail.com>
2025-08-05 22:00:15 +0500

    added create artifact step

--- task15/jenkins_agent.sh
+++ task15/jenkins_agent.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-# Download and install Jenkins:
+# Install Java
 sudo apt update -y
 sudo apt install -y openjdk-21-jre-headless
 
+# Add Jenkins repo and install Jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y jenkins
+sudo apt update
+sudo apt install -y jenkins
 
 sudo systemctl start jenkins
 
 
-# Download and install Node.js:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# Add Node.js 22.x repo (system-wide)
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
 
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
-
-# Download and install Node.js:
-nvm install 22
-
-# Verify the Node.js version:
-node -v # Should print "v22.18.0".
-nvm current # Should print "v22.18.0".
-
-# Verify npm version:
-npm -v # Should print "10.9.3".
+# Install Node.js and npm
+sudo apt install -y nodejs
