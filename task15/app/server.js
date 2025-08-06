552ff954a9d0fad60a6baa9f0941d8be43fb31fd
haiderzaidi07 <haiderzaidi.work@gmail.com>
2025-08-06 20:06:47 +0500

    added conditional to start/restart app

--- task15/app/server.js
+++ task15/app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello AWS EC2 from Jenkins!');
+  res.send('Hello AWS EC2!');
 });
 
 app.listen(PORT, () => {
--- task15/deploy.sh
+++ task15/deploy.sh
@@ -3,5 +3,13 @@
 # Extract app files from tar artifact
 tar -xvzf nodejs-app.tar.gz
 
-# Start Node.js server via PM2
-pm2 start server.js -f
+SCRIPT_NAME="server"
+
+# Start Node.js server via PM2 or Restart if already running
+if pm2 list | grep -q "$SCRIPT_NAME"; then
+    echo "$SCRIPT_NAME.js is already running. Restarting..."
+    pm2 restart "$SCRIPT_NAME.js"
+else
+    echo "$SCRIPT_NAME.js is not running. Starting..."
+    pm2 start "$SCRIPT_NAME.js"
+fi
